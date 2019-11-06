df25cf90a1f114ac82c7b5d22778fe9014b07fdb
Marc Manley <marc@marcmanley.com>
2019-11-06 15:13:04 -0800

    test.py

--- test.py
+++ test.py
@@ -1,3 +1,16 @@
 # this is a test doc
 
-print("Test 3")
+sampleList = [1, 2, 3, 4, 5]
+print("Here's our initial list:")
+print(sampleList)
+print(type(sampleList))
+theTotalSum = sum(sampleList)
+print("Here's the sum of that list:")
+print(theTotalSum)
+print("That's the way you do it!")
+sampleList.append(6)
+print("Here's our updated list:")
+print(sampleList)
+theTotalSum = sum(sampleList)
+print("Here's our new total:")
+print(theTotalSum)
